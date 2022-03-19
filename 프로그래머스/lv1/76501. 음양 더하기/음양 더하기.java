1104cb9ba10d14f9b2bf94d2d60f294049dcda31
variety82 <78781975+variety82@users.noreply.github.com>
2022-03-20 02:54:51 +0900

    [level 1] Title: 음양 더하기, Time: 0.07 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for (int i=0; i<signs.length; i++){
+            if (signs[i]==true){
+                answer += absolutes[i];
+            }
+            if (signs[i]==false){
+                answer -= absolutes[i];
+            }
+        }
+        return answer;
+    }
+}
