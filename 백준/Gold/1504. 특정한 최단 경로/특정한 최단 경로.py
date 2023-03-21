b9794eec798cce69bc9b1f8f9b5fe8e1ad163803
ChanghyunKim <78781975+variety82@users.noreply.github.com>
2023-03-22 08:43:38 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 604 ms, Memory: 132492 KB -BaekjoonHub

---
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
@@ -0,0 +1,39 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+INF = int(1e9)
+N, E = map(int, input().split())
+
+graph = [[] for _ in range(N + 1)]
+
+for _ in range(E):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
+    graph[b].append((a, c))
+
+v1, v2 = map(int, input().split())
+
+def dijkstra(start, end, N):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance = [INF] * (N + 1)
+    distance[start] = 0
+    while(q):
+        dist , now = heapq.heappop(q)
+        if(dist < distance[now]):
+            continue
+        for b, c in graph[now]:
+            cost = dist + c
+            if(cost < distance[b]):
+                distance[b] = cost
+                heapq.heappush(q, (cost,  b))
+    return distance[end]
+
+case1 = dijkstra(1, v1, N) + dijkstra(v1, v2, N) + dijkstra(v2, N, N)
+case2 = dijkstra(1, v1, N) + dijkstra(v1, v2, N) * 2 + dijkstra(v1, N, N)
+case3 = dijkstra(1, v2, N) + dijkstra(v2, v1, N) + dijkstra(v1, N, N)
+case4 = dijkstra(1, v2, N) + dijkstra(v2, v1, N) * 2 + dijkstra(v2, N, N)
+answer = min(case1, case2, case3, case4)
+
+print(-1 if answer >= INF else answer)
