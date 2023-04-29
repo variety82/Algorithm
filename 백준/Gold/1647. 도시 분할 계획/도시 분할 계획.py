113e4ddd92a8bb233a9f3a1be9dc66999508bf25
ChanghyunKim <78781975+variety82@users.noreply.github.com>
2023-04-29 12:39:43 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 2500 ms, Memory: 278272 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
@@ -0,0 +1,33 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+parent = [x for x in range(0, N + 1)]
+edges = [list(map(int, input().split())) for _ in range(M)]
+edges.sort(key = lambda x : x[2])
+
+def find(parent, x):
+    if(x != parent[x]):
+        parent[x] = find(parent, parent[x])
+    return parent[x]
+
+def union(parent, a, b):
+    a = find(parent, a)
+    b = find(parent, b)
+    if(a < b):
+        parent[b] = a
+    else:
+        parent[a] = b
+
+def kruskal(parent, edges):
+    cost = 0
+    _max = 0
+    for edge in edges:
+        a, b, c = edge
+        if(find(parent, a) != find(parent, b)):
+            union(parent, a, b)
+            cost += c
+            _max = c
+    return cost - _max
+
+print(kruskal(parent, edges))
