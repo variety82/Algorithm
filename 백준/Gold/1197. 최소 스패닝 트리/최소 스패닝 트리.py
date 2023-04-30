e2d6db62c9b0db01e0c5bc5443b85966e379e95b
ChanghyunKim <78781975+variety82@users.noreply.github.com>
2023-04-30 16:17:44 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 376 ms, Memory: 130356 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -0,0 +1,31 @@
+import sys
+input = sys.stdin.readline
+
+V, E = map(int, input().split())
+parent = [x for x in range(V + 1)]
+edges = [tuple(map(int, input().split())) for _ in range(E)]
+
+def find(parent, x):
+    if(x != parent[x]):
+        parent[x] = find(parent, parent[x])
+    return parent[x]
+
+def union(parent, a, b):
+    a = find(parent, a)
+    b = find(parent, b)
+    if(a < b):
+        parent[b] = a
+    else:
+        parent[a] = b
+
+def kruskal(parent, edges):
+    edges.sort(key = lambda x : x[2])
+    cost = 0
+    for edge in edges:
+        a, b, c = edge
+        if(find(parent, a) != find(parent, b)):
+            union(parent, a, b)
+            cost += c
+    return cost
+
+print(kruskal(parent, edges))
