68920e8eb4ceeb255906ef3ccdb79716c8ffccf5
ChanghyunKim <78781975+variety82@users.noreply.github.com>
2023-05-23 12:18:18 +0900

    [Gold II] Title: Immortal Porpoises, Time: 200 ms, Memory: 116388 KB -BaekjoonHub

---
+++ 백준/Gold/11524. Immortal Porpoises/Immortal Porpoises.py
@@ -0,0 +1,33 @@
+import sys
+input = sys.stdin.readline
+
+
+fibo_matrix = [[1, 1], [1, 0]]
+
+
+def multiple_matrix(matrix1, matrix2):
+    res = [[0, 0], [0, 0]]
+    for i in range(2):
+        for j in range(2):
+            for k in range(2):
+                res[i][j] = (res[i][j] + matrix1[i][k]
+                             * matrix2[k][j]) % int(1e9)
+    return res
+
+
+def pow(a, n):
+    if (n == 1):
+        return a
+    else:
+        x = pow(a, n // 2)
+        if (n % 2 == 0):
+            return multiple_matrix(x, x)
+        else:
+            return multiple_matrix(multiple_matrix(x, x), a)
+
+
+T = int(input())
+for tc in range(T):
+    _, n = map(int, input().split())
+    fibo = pow(fibo_matrix, n)
+    print(f"{tc + 1} {fibo[0][1]}")
