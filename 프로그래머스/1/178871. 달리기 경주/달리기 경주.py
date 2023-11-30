cc09474d81e2eeb2a5d86602fa7f371ad60d19d2
ChanghyunKim <78781975+variety82@users.noreply.github.com>
2023-11-30 19:44:54 +0900

    [level 1] Title: 달리기 경주, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.py
@@ -1,15 +1,9 @@
 def solution(players, callings):
     palyer_idx_dict = {player:idx for idx, player in enumerate(players)}
-    idx_player_dict = {idx:player for idx, player in enumerate(players)}
     for call in callings:
-        cur_idx = palyer_idx_dict[call]
-        pre_idx = cur_idx - 1
-        pre_player = idx_player_dict[pre_idx]
+        idx = palyer_idx_dict[call]
+        palyer_idx_dict[call] -= 1
+        palyer_idx_dict[players[idx-1]] += 1
 
-        palyer_idx_dict[call] = pre_idx
-        palyer_idx_dict[pre_player] = cur_idx
-
-        idx_player_dict[cur_idx] = pre_player
-        idx_player_dict[pre_idx] = call
-    
-    return list(idx_player_dict.values())
+        players[idx], players[idx-1] = players[idx-1], players[idx]
+    return players
