88bfa8950856a164dea038aee0b56e14974f0930
ChanghyunKim <78781975+variety82@users.noreply.github.com>
2023-12-13 12:39:27 +0900

    [level unrated] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 0.09 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
+++ 프로그래머스/unrated/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.py
@@ -1,31 +1,7 @@
-def sort_by_ext(data, ext, val_ext):
-    idx = -1
-    if ext == 'code':
-        idx = 0
-    elif ext == 'date':
-        idx = 1
-    elif ext == 'maximum':
-        idx = 2
-    else:
-        idx = 3
-    data.sort(key = lambda x : x[idx])
-    for cur_idx, value in enumerate(data):
-        if value[idx] > val_ext:
-            return data[:cur_idx]
-
-def sort_by_str(data, sort_by):
-    idx = -1
-    if sort_by == 'code':
-        idx = 0
-    elif sort_by == 'date':
-        idx = 1
-    elif sort_by == 'maximum':
-        idx = 2
-    else:
-        idx = 3
-    return sorted(data, key = lambda x : x[idx])
-
 def solution(data, ext, val_ext, sort_by):
-    data = sort_by_ext(data, ext, val_ext)
-    data = sort_by_str(data, sort_by)
-    return data
+    answer = []
+    case = {'code':0, 'date':1, 'maximum':2, 'remain':3}
+    for single_data in data:
+        if single_data[case.get(ext)] < val_ext:
+            answer.append(single_data)
+    return sorted(answer, key = lambda x :x[case.get(sort_by)])
