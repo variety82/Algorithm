dfa49a19c64785371fcf63599d4b7d02ef561ed3
ChanghyunKim <78781975+variety82@users.noreply.github.com>
2023-12-27 17:52:35 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.13 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -1,42 +1,21 @@
-def caclulate_date(today, privacie, terms_dict):
-    date, kind = privacie.split()
-    expiration_date = terms_dict[kind]
+def time_to_day(date):
     year, month, day = map(int, date.split("."))
-    q, r = divmod(month + expiration_date, 12)
-    if (month + expiration_date) % 12 == 0:
-        q -= 1
-    year += q
-    month = r if r != 0 else 12
-    if day - 1 == 0:
-        if month - 1 == 0:
-            year -= 1
-            month = 12
-        else:
-            month -= 1
-        day = 28
-    else:
-        day -= 1
-    today_year, today_month, today_day = map(int, today.split("."))
-    if today_year > year:
-        return False
-    if today_year == year and today_month > month:
-        return False
-    if today_year == year and today_month == month and today_day > day:
-        return False
-    return True
+    return year * 12 * 28 + month * 28 + day
 
 def terms_arr_to_dict(terms):
     terms_dict = {}
     for term in terms:
         kind, num = term.split()
-        terms_dict[kind] = int(num)
+        terms_dict[kind] = int(num) * 28
     return terms_dict
-
 
 def solution(today, terms, privacies):
     answer = []
     terms_dict = terms_arr_to_dict(terms)
-    for idx, privacie in enumerate(privacies):
-        if not caclulate_date(today, privacie, terms_dict):
+    today = time_to_day(today)
+    for idx, privacy in enumerate(privacies):
+        date, kind = privacy.split()
+        expiration_date = time_to_day(date) + terms_dict[kind]
+        if expiration_date <= today:
             answer.append(idx + 1)
     return answer
